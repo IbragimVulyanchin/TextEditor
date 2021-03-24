e63dfad579ef4ca6715c888cd14454a17e450800
IbragimVulyanchin <81296913+IbragimVulyanchin@users.noreply.github.com>
2021-03-24 15:34:29 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import sun.tools.jconsole.inspector.XTextFieldEditor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new TextEditor();
+
+    }
+}
